0f4d17c91f22d5111501b3639cab451e938eff3e
Youssef Osman <78634572+youssefahmed3@users.noreply.github.com>
2025-06-07 15:07:17 +0300

    refactoring and adding frontend
    i did saparaction of backend and created a new frontend folder that will serve the frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import { toNodeHandler } from "better-auth/node";
-import test from './routes/test.routes';
-import user from './routes/user.routes';
 import { auth } from './utils/auth';
 import shortUrl from './routes/shortUrl.routes';
 import morgan from 'morgan';
@@ -13,7 +11,6 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(cookieParser());
-console.log("AUTH HANDLER:", typeof auth.handler);
 
 app.all("/api/auth/*", toNodeHandler(auth));
 
@@ -21,14 +18,12 @@ app.use(express.json()); // to make sure that the req.body is JSON {middleware}
 
 
 
-app.use(`/test`, test)
-app.use(`/user`, user)
 app.use(`/shortUrl`, shortUrl)
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('API is working correctly');
 });
 
 
